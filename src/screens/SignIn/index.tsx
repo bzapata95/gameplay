0105233c405436c6b437a7098d5eb7bff63e717e
Bryan Zapata <bmzc95@gmail.com>
2021-06-21 11:22:20 -0500

    day 1

---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { View, Text, Image } from "react-native";
+
+import { ButtonIcon } from "../../components/ButtonIcon";
+
+import IllustrationImg from "../../assets/illustration.png";
+
+import { styles } from "./styles";
+
+export function SignIn() {
+  return (
+    <View style={styles.container}>
+      <Image
+        source={IllustrationImg}
+        style={styles.image}
+        resizeMode="stretch"
+      />
+
+      <View style={styles.content}>
+        <Text style={styles.title}>
+          Organice {"\n"}
+          sus tiempos de juego {"\n"}
+          facilmente
+        </Text>
+
+        <Text style={styles.subtitle}>
+          Crea grupos para jugar sus juegos {"\n"}
+          favoritos con sus amigos
+        </Text>
+
+        <ButtonIcon title="Entrar con Discord" activeOpacity={0.7} />
+      </View>
+    </View>
+  );
+}
